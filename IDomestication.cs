7485565384fddc5ad40ea5e0ff5f32d1f7df09a4
Stutijain98 <60315630+Stutijain98@users.noreply.github.com>
2020-03-26 18:38:30 +0530

    Add files via upload

---
+++ IDomestication.cs
@@ -0,0 +1,16 @@
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    interface IDomestication
+    { 
+        void Use();
+       
+    }
+}
