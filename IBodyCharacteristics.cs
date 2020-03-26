ef5a59244e05e04ffe240e10d84905be5c610a0e
Stutijain98 <60315630+Stutijain98@users.noreply.github.com>
2020-03-26 18:31:30 +0530

    Add files via upload

---
+++ IBodyCharacteristics.cs
@@ -0,0 +1,20 @@
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    interface IBodyCharacteristics
+    { 
+        void Trunk();
+        void Tusk();
+        void Teeth();
+        void Skin();
+        void Legs();
+        void Ears();
+    }
+}
